08c54922d637e7de7729d5e7eaad80f5c8e2cbbb
Guan <search597@outlook.com>
2020-12-28 14:34:38 +0800

    Displaying datatype in console
    Added a file which is a program that displaying a set of digit into console.

---
+++ Datatype_in_console/Datatype_in_console/Module1.vb
@@ -0,0 +1,47 @@
+﻿Module Module1
+    Dim s1 As Single
+    Dim s2 As Double
+    Sub Main()
+        s1 = 1.1234567 : s2 = 1.1234567
+        Console.WriteLine("Orignal Data: 1.1234567")
+        Console.WriteLine("Signal: {0}", s1)
+        Console.WriteLine("Double: {0}", s2)
+        Console.WriteLine("============")
+        s1 = 1234567890 : s2 = 1234567890
+        Console.WriteLine("Orignal Data: 1234567890")
+        Console.WriteLine("Signal: {0}", s1)
+        Console.WriteLine("Double: {0}", s2)
+        Console.WriteLine("============")
+        s1 = 0.123456789 : s2 = 0.123456789
+        Console.WriteLine("Orignal Data: 0.123456789")
+        Console.WriteLine("Signal: {0}", s1)
+        Console.WriteLine("Double: {0}", s2)
+        Console.WriteLine("============")
+        s1 = 0.00000000123456789 : s2 = 0.00000000123456789
+        Console.WriteLine("Orignal Data: 0.00000000123456789")
+        Console.WriteLine("Signal: {0}", s1)
+        Console.WriteLine("Double: {0}", s2)
+        Console.WriteLine("============")
+        s1 = 123456789012 : s2 = 123456789012
+        Console.WriteLine("Orignal Data: 123456789012")
+        Console.WriteLine("Signal: {0}", s1)
+        Console.WriteLine("Double: {0}", s2)
+        Console.WriteLine("============")
+        Dim a As String = "Hello World!"
+        Console.WriteLine(a)
+        Dim b As Char = "B"
+        Console.WriteLine(b)
+
+        Dim bb As Date = #2/14/2007#
+        bb = #2/5/2007 11:35:22 AM#
+        Console.WriteLine(bb)
+
+        'boolean
+
+        Dim Goo As Boolean
+        Goo = 35 < 25
+        Console.WriteLine(Goo)
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Datatype_in_console/Datatype_in_console/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Datatype_in_console")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Datatype_in_console")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("05ba384c-ee63-420f-9b4b-105b5f12771e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
